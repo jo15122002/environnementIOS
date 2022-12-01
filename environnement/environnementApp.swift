733fbb98085b02e8936d3ac977768d2f9ceb8bfd
digital <digital@iMac24-7.local>
2022-12-01 09:17:38 +0100

    init

---
+++ environnement/environnementApp.swift
@@ -0,0 +1,17 @@
+//
+//  environnementApp.swift
+//  environnement
+//
+//  Created by digital on 01/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct environnementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
